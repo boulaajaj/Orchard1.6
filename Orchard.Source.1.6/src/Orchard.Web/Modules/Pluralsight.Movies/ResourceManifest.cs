64b7718392d93759ef1afcd3edcc468416cf5096
Rich Smith <richloveswork@gmail.com>
2012-12-27 15:56:23 +1100

    added bootstrap and mainfest files

---
+++ Orchard.Source.1.6/src/Orchard.Web/Modules/Pluralsight.Movies/ResourceManifest.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.UI.Resources;
+
+namespace Pluralsight.Movies
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+            manifest.DefineStyle("Movies").SetUrl("Pluralsight-Movies.css");
+        }
+    }
+}
