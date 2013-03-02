7730ea8f1f4a9fbde597f70c633014586b05e63f
rich smith <rich__smith@hotmail.com>
2013-03-02 11:07:43 +1100

    removed mello

---
+++ Orchard.Source.1.6/src/Orchard.Web/Modules/Richinoz.Paypal/AdminMenu.cs
@@ -0,0 +1,40 @@
+﻿using Orchard.Localization;
+using Orchard.UI.Navigation;
+
+namespace Richinoz.Paypal
+{
+    public class AdminMenu : INavigationProvider
+    {
+        public Localizer T { get; set; }
+
+        public string MenuName
+        {
+            get { return "admin"; }
+        }
+
+        public void GetNavigation(NavigationBuilder builder)
+        {
+            builder.Add(T("Paypal"), "5", BuildMenu);
+        }
+
+        private void BuildMenu(NavigationItemBuilder menu)
+        {
+           // Admin/Contents/List/Movie
+            menu.Add(T("List"), "1.0", item =>
+                item.Action("List", "Admin", new { area = "Contents", id = "Product" }));
+
+            //menu.Add(T("New Movie"), "1.1", item =>
+            //    item.Action("Create", "Admin", new { area = "Contents", id = "Movie" }));
+
+            //menu.Add(T("Movie Lookup"), "1.2", item =>
+            //    item.Action("Index", "MovieLookup", new { area = "Pluralsight.Movies" }).Permission(Permissions.LookupMovie));
+
+            //menu.Add(T("Actors"), "2.0", item =>
+            //    item.Action("Index", "ActorsAdmin", new { area = "Pluralsight.Movies" }));
+
+            ////Admin/Contents/List/PrettyGallery
+            // menu.Add(T("Image Gallery"), "3.0", item =>
+            //    item.Action("List", "Admin", new { area = "Contents", id = "PrettyGallery" }));
+        }
+    }
+}
