85faf0d5099fcd32e44fb4b928052adeec22683d
Rich Smith <richloveswork@gmail.com>
2013-03-12 10:18:07 +1100

    order menu

---
+++ Orchard.Source.1.6/src/Orchard.Web/Modules/Richinoz.Paypal/ProductAdminMenu.cs
@@ -0,0 +1,35 @@
+﻿using Orchard.Environment.Extensions;
+using Orchard.Localization;
+using Orchard.UI.Navigation;
+
+namespace Richinoz.Paypal {
+   // [OrchardFeature("Nwazet.Commerce")]
+    public class OrderAdminMenu : INavigationProvider {
+        public string MenuName {
+            get { return "admin"; }
+        }
+
+        public OrderAdminMenu()
+        {
+            T = NullLocalizer.Instance;
+        }
+
+        private Localizer T { get; set; }
+
+        public void GetNavigation(NavigationBuilder builder) {
+            builder
+                .AddImageSet("modules")
+                .Add(item => item
+                    .Caption(T("Orders"))
+                    .Position("2")
+                    .LinkToFirstChild(true)
+
+                    .Add(subItem => subItem
+                        .Caption(T("Orders"))
+                        .Position("2.0")
+                        .Action("List", "OrderAdmin", new { area = "Richinoz.Paypal" })
+                    )
+                );
+        }
+    }
+}
