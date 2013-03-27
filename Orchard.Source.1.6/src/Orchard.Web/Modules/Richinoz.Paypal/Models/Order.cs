eac1cf76a9f5e7bce9ec943dcefafbe830e777f9
Rich Smith <richloveswork@gmail.com>
2013-03-27 17:03:39 +1100

    placement.info

--- Orchard.Source.1.6/src/Orchard.Web/Modules/Richinoz.Paypal/Models/Order.cs
+++ Orchard.Source.1.6/src/Orchard.Web/Modules/Richinoz.Paypal/Models/Order.cs
@@ -9,9 +9,6 @@ namespace Richinoz.Paypal.Models
     [Serializable]
     public class Order : IOrder
     {
-        private decimal _amount;
-        private decimal _amountPaid;
-        private string _transactionId;
         public Order()
         {
             OrderItems = new List<IOrderItem>();
@@ -21,19 +18,8 @@ namespace Richinoz.Paypal.Models
         public List<IOrderItem> OrderItems { get; set; }
         public IAddress Address { get; set; }
 
-        public decimal OriginalAmount {
-            get { return _amount; }
-            set { _amount = value; }
-        }
-
-        public decimal AmountPaid {
-            get { return _amountPaid; }
-            set { _amountPaid = value; }
-        }
-
-        public string TransactionId {
-            get { return _transactionId; }
-            set { _transactionId = value; }
-        }
+        public decimal OriginalAmount { get; set; }
+        public decimal AmountPaid { get; set; }
+        public string TransactionId { get; set; }
     }
 }
